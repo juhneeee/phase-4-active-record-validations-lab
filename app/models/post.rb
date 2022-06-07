72e3e203ab4e97eb940fe21524c0ceb9cd94cc2f
Johnny Chen <johnnyc1314@gmail.com>
2022-06-07 11:05:48 -0400

    commit tree 9e4aa4bb2f82e0360652fc6c8a7cdbb90b723620

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :must_have_clickbait
+    
+    def must_have_clickbait
+        clickbaits = [/won't believe/i, /secret/i, /top \d+/i, /guess/i]
+        clickbaits.each do |c|
+            if title
+                if title.match(c)
+                    return true
+                end
+            end
+        end
+        errors.add(:title, "Title not clickbaity.")
+    end
 end
